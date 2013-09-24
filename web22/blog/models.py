df9b705098fdee6c496afd546169858d9026b2a2
bchpan2013 <bchpan2013@163.com>
2013-09-24 11:25:46 +0800

    add jquery

--- web22/blog/models.py
+++ web22/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+form django.contrib import User
+
 
 class BookCategory(models.Model):
     category = models.CharField(max_length=20)
@@ -7,3 +9,26 @@ class BookCategory(models.Model):
     def __unicode__(self):
         return self.category
 
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    price = models.FloatField()
+    category = models.ForeignKey(BookCategory)
+    
+    def __unicode__(self):
+        return self.name
+
+"""
+class CarShop(models.Model):
+    user = models.OneToOneField()
+    books = models.ManyToMany(Book)
+    
+    def add_book(self, book):
+        pass
+
+    def remove_book(self, book):
+        pass
+    def book_sum()
+	pass
+    def clear_car()
+
+"""
--- web22/blog/views.py
+++ web22/blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from blog.models import BookCategory
+from blog.models import BookCategory, Book
 
 def index(req):
+    book_list = Book.objects.all()
     book_category_list = BookCategory.objects.all()
     first_book_category_list = [b for b in book_category_list if b.p_category is None]
     print first_book_category_list
-    return render(req, 'index.html', {'first_book_category_list':first_book_category_list})
+    return render(req, 'index.html', {'book_list':book_list,'first_book_category_list':first_book_category_list})
 
